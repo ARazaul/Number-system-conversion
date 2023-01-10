7fb2bf26ae1576cc00110be48cc9fc4faf47a81b
Ansari MD <103068419+RajaulAnsari@users.noreply.github.com>
2023-01-10 22:53:18 +0545

    Update b2d.py

--- b2d.py
+++ b2d.py
@@ -8,24 +8,12 @@ d=160
 print("hex: ",hex(d))
 
 
-
 print('hex to decimal')
 h='16'
 print("dec: ",int(h,base=16))
 
 
-
-print('decimal to binary')
-d=7
-print("bin: ",bin(d))
-
 print('decimal to binary')
 d1=200
 print(bin(d1))
-
-
-print('hex to binary ')
-h1='AC'
-print(bin(h1,base=2))
-
 
